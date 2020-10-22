8e21634b833d045b7ac997afdcecbd0703294c88
David Beck <david.beck@bexoft.de>
2020-10-22 02:43:57 -0700

    Add tests for MetaDataExchange.

--- src/metadataserialize/MetaDataExchange.cpp
+++ src/metadataserialize/MetaDataExchange.cpp
@@ -1,316 +1,88 @@
-#include "metadataserialize/StructsMetaData.h"
+#include "metadataserialize/MetaDataExchange.h"
+#include "metadata/MetaData.h"
+
+#include <algorithm>
 
 
 
-SerializeMetaTypeId::SerializeMetaTypeId()
+void MetaDataExchange::importMetaData(const SerializeMetaData& metadata)
 {
-}
-SerializeMetaTypeId::SerializeMetaTypeId(Enum en)
-    : m_value(en)
-{
-}
-SerializeMetaTypeId::operator const Enum&() const
-{
-    return m_value;
-}
-SerializeMetaTypeId::operator Enum&()
-{
-    return m_value;
-}
-const SerializeMetaTypeId& SerializeMetaTypeId::operator =(Enum en)
-{
-    m_value = en;
-    return *this;
-}
-const std::string& SerializeMetaTypeId::toString() const
-{
-    return _enumInfo.getMetaEnum().getNameByValue(m_value);
-}
-void SerializeMetaTypeId::fromString(const std::string& name)
-{
-    m_value = static_cast<Enum>(_enumInfo.getMetaEnum().getValueByName(name));
-}
-const EnumInfo SerializeMetaTypeId::_enumInfo = {
-    "SerializeMetaTypeId", "description", {
-        {"TYPE_NONE",    TYPE_NONE, "description"},
-        {"TYPE_BOOL",    TYPE_BOOL, "description"},
-        {"TYPE_INT32",   TYPE_INT32, "description"},
-        {"TYPE_UINT32",  TYPE_UINT32, "description"},
-        {"TYPE_INT64",   TYPE_INT64, "description"},
-        {"TYPE_UINT64",  TYPE_UINT64, "description"},
-        {"TYPE_FLOAT",   TYPE_FLOAT, "description"},
-        {"TYPE_DOUBLE",  TYPE_DOUBLE, "description"},
-        {"TYPE_STRING",  TYPE_STRING, "description"},
-        {"TYPE_BYTES",   TYPE_BYTES, "description"},
-        {"TYPE_STRUCT",  TYPE_STRUCT, "description"},
-        {"TYPE_ENUM",    TYPE_ENUM, "description"},
+    // enums
+    for (size_t i = 0; i < metadata.enums.size(); ++i)
+    {
+        const SerializeMetaEnum& enumSource = metadata.enums[i];
+        std::vector<MetaEnumEntry> entries;
+        for (size_t n = 0; n < enumSource.entries.size(); ++n)
+        {
+            const SerializeMetaEnumEntry& entrySource = enumSource.entries[i];
+            entries.push_back({entrySource.name, entrySource.id, entrySource.description});
+        }
+        MetaDataGlobal::instance().addEnum({enumSource.typeName, enumSource.description, std::move(entries)});
+    }
 
-        {"OFFSET_ARRAY_FLAG", OFFSET_ARRAY_FLAG, "description"},
+    // structs
+    for (size_t i = 0; i < metadata.structs.size(); ++i)
+    {
+        const SerializeMetaStruct& structSource = metadata.structs[i];
+        std::vector<MetaField> fields;
+        for (size_t n = 0; n < structSource.fields.size(); ++n)
+        {
+            const SerializeMetaField& fieldSource = structSource.fields[i];
+            int flags = 0;
+            std::for_each(fieldSource.flags.begin(), fieldSource.flags.end(), [&flags] (const SerializeMetaFieldFlags& flag) {
+                flags |= flag;
+            });
+            fields.push_back({fieldSource.typeId, fieldSource.typeName, fieldSource.name, fieldSource.description, flags});
+        }
+        MetaDataGlobal::instance().addStruct({structSource.typeName, structSource.description, std::move(fields)});
+    }
+}
 
-        {"TYPE_ARRAY_BOOL",    TYPE_ARRAY_BOOL, "description"},
-        {"TYPE_ARRAY_INT32",   TYPE_ARRAY_INT32, "description"},
-        {"TYPE_ARRAY_UINT32",  TYPE_ARRAY_UINT32, "description"},
-        {"TYPE_ARRAY_INT64",   TYPE_ARRAY_INT64, "description"},
-        {"TYPE_ARRAY_UINT64",  TYPE_ARRAY_UINT64, "description"},
-        {"TYPE_ARRAY_FLOAT",   TYPE_ARRAY_FLOAT, "description"},
-        {"TYPE_ARRAY_DOUBLE",  TYPE_ARRAY_DOUBLE, "description"},
-        {"TYPE_ARRAY_STRING",  TYPE_ARRAY_STRING, "description"},
-        {"TYPE_ARRAY_BYTES",   TYPE_ARRAY_BYTES, "description"},
-        {"TYPE_ARRAY_STRUCT",  TYPE_ARRAY_STRUCT, "description"},
-        {"TYPE_ARRAY_ENUM",    TYPE_ARRAY_ENUM, "description"},
-     }
-};
+void MetaDataExchange::exportMetaData(SerializeMetaData& metadata)
+{
+    metadata.enums.clear();
+    metadata.structs.clear();
 
+    // enums
+    const std::unordered_map<std::string, MetaEnum>& enumsSource = MetaDataGlobal::instance().getAllEnums();
+    for (auto it = enumsSource.begin(); it != enumsSource.end(); ++it)
+    {
+        const MetaEnum& enumSource = it->second;
+        SerializeMetaEnum enumDestination;
+        enumDestination.typeName = enumSource.getTypeName();
+        enumDestination.description = enumSource.getDescription();
+        for (int n = 0; n < enumSource.getEntrySize(); ++n)
+        {
+            const MetaEnumEntry* entrySource = enumSource.getEntryByIndex(n);
+            assert(entrySource);
+            enumDestination.entries.push_back({entrySource->name, entrySource->id, entrySource->description});
+        }
+        metadata.enums.push_back(std::move(enumDestination));
+    }
 
-
-
-SerializeMetaFieldFlags::SerializeMetaFieldFlags()
-{
+    // structs
+    const std::unordered_map<std::string, MetaStruct>& structsSource = MetaDataGlobal::instance().getAllStructs();
+    for (auto it = structsSource.begin(); it != structsSource.end(); ++it)
+    {
+        const MetaStruct& structSource = it->second;
+        SerializeMetaStruct structDestination;
+        structDestination.typeName = structSource.getTypeName();
+        structDestination.description = structSource.getDescription();
+        for (int n = 0; n < structSource.getFieldsSize(); ++n)
+        {
+            const MetaField* fieldSource = structSource.getFieldByIndex(n);
+            assert(fieldSource);
+            std::vector<SerializeMetaFieldFlags> flags;
+            for (int k = 0; k < 32; ++k)
+            {
+                std::int32_t flag = 1 << k;
+                if (fieldSource->flags & flag)
+                {
+                    flags.push_back(static_cast<SerializeMetaFieldFlags::Enum>(flag));
+                }
+            }
+            structDestination.fields.push_back({fieldSource->typeId, fieldSource->typeName, fieldSource->name, fieldSource->description, flags});
+        }
+        metadata.structs.push_back(std::move(structDestination));
+    }
 }
-SerializeMetaFieldFlags::SerializeMetaFieldFlags(Enum en)
-    : m_value(en)
-{
-}
-SerializeMetaFieldFlags::operator const Enum&() const
-{
-    return m_value;
-}
-SerializeMetaFieldFlags::operator Enum&()
-{
-    return m_value;
-}
-const SerializeMetaFieldFlags& SerializeMetaFieldFlags::operator =(Enum en)
-{
-    m_value = en;
-    return *this;
-}
-const std::string& SerializeMetaFieldFlags::toString() const
-{
-    return _enumInfo.getMetaEnum().getNameByValue(m_value);
-}
-void SerializeMetaFieldFlags::fromString(const std::string& name)
-{
-    m_value = static_cast<Enum>(_enumInfo.getMetaEnum().getValueByName(name));
-}
-const EnumInfo SerializeMetaFieldFlags::_enumInfo = {
-    "SerializeMetaFieldFlags", "description", {
-        {"METAFLAG_NONE",           METAFLAG_NONE, "description"},
-        {"METAFLAG_PROTO_VARINT",   METAFLAG_PROTO_VARINT, "description"},
-        {"METAFLAG_PROTO_ZIGZAG",   METAFLAG_PROTO_ZIGZAG, "description"},
-     }
-};
-
-
-
-
-
-
-SerializeMetaEnumEntry::SerializeMetaEnumEntry()
-{
-}
-SerializeMetaEnumEntry::SerializeMetaEnumEntry(const decltype(name)& name_, const decltype(id)& id_, const decltype(description)& description_)
-    : name(name_)
-    , id(id_)
-    , description(description_)
-{
-}
-void SerializeMetaEnumEntry::clear()
-{
-    *this = SerializeMetaEnumEntry();
-}
-const StructInfo& SerializeMetaEnumEntry::getStructInfo() const
-{
-    return _structInfo;
-}
-bool SerializeMetaEnumEntry::operator ==(const SerializeMetaEnumEntry& rhs) const
-{
-    return (name == rhs.name &&
-            id == rhs.id &&
-            description == rhs.description);
-}
-bool SerializeMetaEnumEntry::operator !=(const SerializeMetaEnumEntry& rhs) const
-{
-    return !(*this == rhs);
-}
-const StructInfo SerializeMetaEnumEntry::_structInfo = {
-    "SerializeMetaEnumEntry", "description", {
-         {TYPE_STRING, "",  "name",         "description", 0},
-         {TYPE_INT32,  "",  "id",           "description", 0},
-         {TYPE_STRING, "",  "description",  "description", 0}
-     },{
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaEnumEntry, name)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaEnumEntry, id)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaEnumEntry, description)}
-     }
-};
-
-
-SerializeMetaEnum::SerializeMetaEnum()
-{
-}
-SerializeMetaEnum::SerializeMetaEnum(const decltype(typeName)& typeName_, const decltype(description)& description_, const decltype(entries)& entries_)
-    : typeName(typeName_)
-    , description(description_)
-    , entries(entries_)
-{
-}
-void SerializeMetaEnum::clear()
-{
-    *this = SerializeMetaEnum();
-}
-const StructInfo& SerializeMetaEnum::getStructInfo() const
-{
-    return _structInfo;
-}
-bool SerializeMetaEnum::operator ==(const SerializeMetaEnum& rhs) const
-{
-    return (typeName == rhs.typeName &&
-            description == rhs.description &&
-            entries == rhs.entries);
-}
-bool SerializeMetaEnum::operator !=(const SerializeMetaEnum& rhs) const
-{
-    return !(*this == rhs);
-}
-const StructInfo SerializeMetaEnum::_structInfo = {
-    "SerializeMetaEnum", "description", {
-        {TYPE_STRING,                             "",   "typeName",     "description", 0},
-        {TYPE_STRING,                             "",   "description",  "description", 0},
-        {TYPE_ARRAY_STRUCT, "SerializeMetaEnumEntry",   "entries",      "description", 0}
-    },{
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaEnum, typeName)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaEnum, description)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaEnum, entries), new ArrayStructAdapter<SerializeMetaEnumEntry>}
-     }
-};
-
-
-
-
-SerializeMetaField::SerializeMetaField()
-{
-}
-SerializeMetaField::SerializeMetaField(const decltype(typeId)& typeId_, const decltype(typeName)& typeName_, const decltype(name)& name_,
-                                       const decltype(description)& description_, const decltype(flags)& flags_)
-    : typeId(typeId_)
-    , typeName(typeName_)
-    , name(name_)
-    , description(description_)
-    , flags(flags_)
-{
-}
-void SerializeMetaField::clear()
-{
-    *this = SerializeMetaField();
-}
-const StructInfo& SerializeMetaField::getStructInfo() const
-{
-    return _structInfo;
-}
-bool SerializeMetaField::operator ==(const SerializeMetaField& rhs) const
-{
-    return (typeId == rhs.typeId &&
-            typeName == rhs.typeName &&
-            name == rhs.name &&
-            description == rhs.description &&
-            flags == rhs.flags);
-}
-bool SerializeMetaField::operator !=(const SerializeMetaField& rhs) const
-{
-    return !(*this == rhs);
-}
-const StructInfo SerializeMetaField::_structInfo = {
-    "SerializeMetaField", "description", {
-        {TYPE_ENUM,         "SerializeMetaTypeId",  "typeId",       "description", 0},
-        {TYPE_STRING,       "",                     "typeName",     "description", 0},
-        {TYPE_STRING,       "",                     "name",         "description", 0},
-        {TYPE_STRING,       "",                     "description",  "description", 0},
-        {TYPE_ARRAY_STRING, "",                     "flags",        "description", 0}
-     },{
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaField, typeId)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaField, typeName)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaField, name)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaField, description)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaField, flags)}
-     }
-};
-
-
-
-SerializeMetaStruct::SerializeMetaStruct()
-{
-}
-SerializeMetaStruct::SerializeMetaStruct(const decltype(typeName)& typeName_, const decltype(description)& description_, const decltype(fields)& fields_)
-    : typeName(typeName_)
-    , description(description_)
-    , fields(fields_)
-{
-}
-void SerializeMetaStruct::clear()
-{
-    *this = SerializeMetaStruct();
-}
-const StructInfo& SerializeMetaStruct::getStructInfo() const
-{
-    return _structInfo;
-}
-bool SerializeMetaStruct::operator ==(const SerializeMetaStruct& rhs) const
-{
-    return (typeName == rhs.typeName &&
-            description == rhs.description &&
-            fields == rhs.fields);
-}
-bool SerializeMetaStruct::operator !=(const SerializeMetaStruct& rhs) const
-{
-    return !(*this == rhs);
-}
-const StructInfo SerializeMetaStruct::_structInfo = {
-    "SerializeMetaStruct", "description", {
-        {TYPE_STRING,                         "", "typeName",     "description", 0},
-        {TYPE_STRING,                         "", "description",  "description", 0},
-        {TYPE_ARRAY_STRUCT, "SerializeMetaField", "fields",       "description", 0}
-    },{
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaStruct, typeName)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaStruct, description)},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaStruct, fields), new ArrayStructAdapter<SerializeMetaField>}
-     }
-};
-
-
-
-
-SerializeMetaData::SerializeMetaData()
-{
-}
-SerializeMetaData::SerializeMetaData(const decltype(enums)& enums_, const decltype(structs)& structs_)
-    : enums(enums_)
-    , structs(structs_)
-{
-}
-void SerializeMetaData::clear()
-{
-    *this = SerializeMetaData();
-}
-const StructInfo& SerializeMetaData::getStructInfo() const
-{
-    return _structInfo;
-}
-bool SerializeMetaData::operator ==(const SerializeMetaData& rhs) const
-{
-    return (enums == rhs.enums &&
-            structs == rhs.structs);
-}
-bool SerializeMetaData::operator !=(const SerializeMetaData& rhs) const
-{
-    return !(*this == rhs);
-}
-const StructInfo SerializeMetaData::_structInfo = {
-    "SerializeMetaData", "description", {
-        {TYPE_ARRAY_STRUCT, "SerializeMetaEnum",   "enums",   "description", 0},
-        {TYPE_ARRAY_STRUCT, "SerializeMetaStruct", "structs", "description", 0}
-    },{
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaData, enums),   new ArrayStructAdapter<SerializeMetaEnum>},
-        {OFFSET_STRUCTBASE_TO_PARAM(SerializeMetaData, structs), new ArrayStructAdapter<SerializeMetaStruct>}
-     }
-};
-
---
+++ test/testMetaDataExchange.cpp
@@ -0,0 +1,157 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+
+#include "metadataserialize/MetaDataExchange.h"
+#include "metadata/MetaData.h"
+
+
+using ::testing::_;
+using ::testing::Return;
+using ::testing::ReturnRef;
+using testing::Invoke;
+using testing::DoAll;
+
+
+
+class TestMetaDataExchange : public testing::Test
+{
+public:
+protected:
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+
+};
+
+
+const std::string ENUM_TYPE = "MyTestEnumType";
+const std::string ENUM_DESCRIPTION = "enum description 0";
+const std::string ENUM_ENTRY_NAME = "ENTRY_TEST1";
+const std::int32_t ENUM_ENTRY_ID = 0;
+const std::string ENUM_ENTRY_DESCRIPTION = "enum description 1";
+
+const std::string STRUCT_TYPE = "MyTestStructType";
+const std::string STRUCT_DESCRIPTION = "struct description";
+const SerializeMetaTypeId STRUCT_ENTRY_TYPEID = TYPE_STRING;
+const std::string STRUCT_ENTRY_TYPENAME = "JustATypeName";
+const std::string STRUCT_ENTRY_NAME = "value";
+const std::string STRUCT_ENTRY_DESCRIPTION = "value description";
+const std::vector<SerializeMetaFieldFlags> STRUCT_ENTRY_FLAGS = {METAFLAG_PROTO_VARINT, METAFLAG_PROTO_ZIGZAG};
+
+
+TEST_F(TestMetaDataExchange, testImportEnum)
+{
+    const MetaEnum* metaEnum = MetaDataGlobal::instance().getEnum(ENUM_TYPE);
+    ASSERT_EQ(metaEnum, nullptr);
+
+    SerializeMetaEnum en;
+
+    en.typeName = ENUM_TYPE;
+    en.description = ENUM_DESCRIPTION;
+    en.entries.push_back({ENUM_ENTRY_NAME, ENUM_ENTRY_ID, ENUM_ENTRY_DESCRIPTION});
+
+    SerializeMetaData serializeMetaData;
+    serializeMetaData.enums.push_back(en);
+
+    MetaDataExchange::importMetaData(serializeMetaData);
+
+    metaEnum = MetaDataGlobal::instance().getEnum(ENUM_TYPE);
+    ASSERT_NE(metaEnum, nullptr);
+    ASSERT_EQ(metaEnum->getTypeName(), ENUM_TYPE);
+    ASSERT_EQ(metaEnum->getDescription(), ENUM_DESCRIPTION);
+    ASSERT_EQ(metaEnum->getEntrySize(), 1);
+    const MetaEnumEntry* enumEntry = metaEnum->getEntryByIndex(0);
+    ASSERT_NE(enumEntry, nullptr);
+    ASSERT_EQ(enumEntry->name, ENUM_ENTRY_NAME);
+    ASSERT_EQ(enumEntry->id, ENUM_ENTRY_ID);
+    ASSERT_EQ(enumEntry->description, ENUM_ENTRY_DESCRIPTION);
+}
+
+TEST_F(TestMetaDataExchange, testImportStruct)
+{
+    const MetaStruct* metaStruct = MetaDataGlobal::instance().getStruct(STRUCT_TYPE);
+    ASSERT_EQ(metaStruct, nullptr);
+
+    SerializeMetaStruct stru;
+
+    stru.typeName = STRUCT_TYPE;
+    stru.description = STRUCT_DESCRIPTION;
+    stru.fields.push_back({STRUCT_ENTRY_TYPEID, STRUCT_ENTRY_TYPENAME, STRUCT_ENTRY_NAME, STRUCT_ENTRY_DESCRIPTION, STRUCT_ENTRY_FLAGS});
+
+    SerializeMetaData serializeMetaData;
+    serializeMetaData.structs.push_back(stru);
+
+    MetaDataExchange::importMetaData(serializeMetaData);
+
+    metaStruct = MetaDataGlobal::instance().getStruct(STRUCT_TYPE);
+    ASSERT_NE(metaStruct, nullptr);
+    ASSERT_EQ(metaStruct->getTypeName(), STRUCT_TYPE);
+    ASSERT_EQ(metaStruct->getDescription(), STRUCT_DESCRIPTION);
+    ASSERT_EQ(metaStruct->getFieldsSize(), 1);
+    const MetaField* field = metaStruct->getFieldByIndex(0);
+    ASSERT_NE(field, nullptr);
+    ASSERT_EQ(field->typeId, STRUCT_ENTRY_TYPEID);
+    ASSERT_EQ(field->typeName, STRUCT_ENTRY_TYPENAME);
+    ASSERT_EQ(field->name, STRUCT_ENTRY_NAME);
+    ASSERT_EQ(field->description, STRUCT_ENTRY_DESCRIPTION);
+    ASSERT_EQ(field->flags, METAFLAG_PROTO_VARINT | METAFLAG_PROTO_ZIGZAG);
+}
+
+
+
+TEST_F(TestMetaDataExchange, testExportEnum)
+{
+    SerializeMetaEnum en;
+    en.typeName = ENUM_TYPE;
+    en.description = ENUM_DESCRIPTION;
+    en.entries.push_back({ENUM_ENTRY_NAME, ENUM_ENTRY_ID, ENUM_ENTRY_DESCRIPTION});
+    SerializeMetaData serializeMetaData;
+    serializeMetaData.enums.push_back(en);
+    MetaDataExchange::importMetaData(serializeMetaData);
+
+    SerializeMetaData serializeMetaDataExport;
+    MetaDataExchange::exportMetaData(serializeMetaDataExport);
+
+    int found = 0;
+    for (size_t i = 0; i < serializeMetaDataExport.enums.size(); ++i)
+    {
+        if (serializeMetaDataExport.enums[i].typeName == ENUM_TYPE)
+        {
+            found++;
+            ASSERT_EQ(serializeMetaDataExport.enums[i], en);
+        }
+    }
+    ASSERT_EQ(found, 1);
+}
+
+
+TEST_F(TestMetaDataExchange, testExportStruct)
+{
+    SerializeMetaStruct stru;
+    stru.typeName = STRUCT_TYPE;
+    stru.description = STRUCT_DESCRIPTION;
+    stru.fields.push_back({STRUCT_ENTRY_TYPEID, STRUCT_ENTRY_TYPENAME, STRUCT_ENTRY_NAME, STRUCT_ENTRY_DESCRIPTION, STRUCT_ENTRY_FLAGS});
+    SerializeMetaData serializeMetaData;
+    serializeMetaData.structs.push_back(stru);
+    MetaDataExchange::importMetaData(serializeMetaData);
+
+    SerializeMetaData serializeMetaDataExport;
+    MetaDataExchange::exportMetaData(serializeMetaDataExport);
+
+    int found = 0;
+    for (size_t i = 0; i < serializeMetaDataExport.structs.size(); ++i)
+    {
+        if (serializeMetaDataExport.structs[i].typeName == STRUCT_TYPE)
+        {
+            found++;
+            ASSERT_EQ(serializeMetaDataExport.structs[i], stru);
+        }
+    }
+    ASSERT_EQ(found, 1);
+}
+
