03e67f90fcc1f9c58fb38ef9bafde8b8ca3dac32
David Beck <david.beck@bexoft.de>
2021-01-08 14:30:35 -0800

    Extract queries.

--- services/fmqfcgi/fmqfcgi.cpp
+++ services/fmqfcgi/fmqfcgi.cpp
@@ -113,6 +113,12 @@ public:
         FCGX_PutStr(data.c_str(), data.size(), m_request->out);
     }
 
+    inline FCGX_Request* operator ->()
+    {
+        assert(m_request);
+        return m_request;
+    }
+
 private:
     Request(const Request&) = delete;
     const Request& operator =(const Request&) = delete;
@@ -122,14 +128,141 @@ private:
 
 
 
-int main(void)
+
+class RequestManager
 {
+public:
 
-    FCGX_Init();
+    static void decode(std::string& str)
+    {
+        const char* src = str.c_str();
+        char* dest = const_cast<char*>(str.c_str());
+        char code[3] = {0};
+        unsigned long c = 0;
 
-    Request request;
+        while (*src)
+        {
+            if (*src == '%')
+            {
+                ++src;
+                memcpy(code, src, 2);
+                c = strtoul(code, NULL, 16);
+                *dest = (char)c;
+                src += 2;
+                dest++;
+            }
+            else
+            {
+                *dest = *src;
+                ++dest;
+                ++src;
+            }
+        }
+        str.resize(dest - str.c_str());
+    }
 
-    while (request.accept() == 0) {
+    void getQueries(const char* querystring, std::vector<std::string>& listQuery)
+    {
+        const char* str = nullptr;
+        while ((str = strchr(querystring, '&')) != nullptr)
+        {
+            std::string query(querystring, str);
+            decode(query);
+            if (!query.empty())
+            {
+                listQuery.push_back(std::move(query));
+            }
+            querystring = str + 1;
+        }
+        std::string query(querystring);
+        decode(query);
+        if (!query.empty())
+        {
+            listQuery.push_back(std::move(query));
+        }
+    }
+
+    void getData(Request& request, std::string& data)
+    {
+        static const int DATABLOCKSIZE = 1000;
+        int cnt = 0;
+        do
+        {
+            int startIx = data.size();
+            data.resize(startIx + DATABLOCKSIZE);
+            cnt = FCGX_GetStr(&data[startIx], DATABLOCKSIZE, request->in);
+        } while (cnt == DATABLOCKSIZE);
+        data.resize(data.size() - DATABLOCKSIZE + cnt);
+    }
+
+
+    void handleRequest(Request& request)
+    {
+        // GET http://localhost/root.fmq/MyService/helloworld.getGreetings%7B%22hello%22:%22world%22%7D?call=hello.world{%22hey%22:5}
+        // REQUEST_URI: /root.fmq/MyService/helloworld.getGreetings%7B%22hello%22:%22world%22%7D?call=hello.world{%22hey%22:5}
+        // PATH_INFO: /MyService/helloworld.getGreetings{"hello":"world"}
+        // QUERY_STRING: call=hello.world%7B%22hey%22:5%7D
+
+        const char* cookies = FCGX_GetParam("HTTP_COOKIE", request->envp);
+        const char* querystring = FCGX_GetParam("QUERY_STRING", request->envp);
+        const char* pathinfo = FCGX_GetParam("PATH_INFO", request->envp);
+        const char* uriEscaped = FCGX_GetParam("REQUEST_URI", request->envp);
+        const char* origin = FCGX_GetParam("HTTP_ORIGIN", request->envp);
+
+        if (uriEscaped)
+        {
+            std::cerr << "REQUEST_URI: " << uriEscaped << std::endl;
+        }
+        if (pathinfo)
+        {
+            std::cerr << "PATH_INFO: " << pathinfo << std::endl;
+        }
+        if (querystring)
+        {
+            std::cerr << "QUERY_STRING: " << querystring << std::endl;
+        }
+        if (cookies)
+        {
+            std::cerr << "HTTP_COOKIE: " << cookies << std::endl;
+        }
+        if (origin)
+        {
+            std::cerr << "HTTP_ORIGIN: " << origin << std::endl;
+        }
+
+        FCGX_FPrintF(request->out, "Content-type: text/plain; charset=utf-8\r\n");
+        FCGX_FPrintF(request->out, "Pragma: no-cache\r\n");
+        FCGX_FPrintF(request->out, "Cache-Control: no-cache\r\n");
+        FCGX_FPrintF(request->out, "Expires: -1\r\n");
+        FCGX_FPrintF(request->out, "Access-Control-Allow-Credentials: true\r\n");
+        if (origin && origin[0] != 0)
+        {
+            std::string strACAO = "Access-Control-Allow-Origin: ";
+            strACAO += origin;
+            strACAO += "\r\n";
+            FCGX_FPrintF(request->out, strACAO.c_str());
+        }
+        else
+        {
+            FCGX_FPrintF(request->out, "Access-Control-Allow-Origin: null\r\n");
+        }
+
+
+        std::vector<std::string> listQuery;
+        if (querystring && querystring[0])
+        {
+            getQueries(querystring, listQuery);
+        }
+
+        std::string data;
+        getData(request, data);
+        getQueries(data.c_str(), listQuery);
+
+        for (size_t i = 0; i < listQuery.size(); ++i)
+        {
+            std::cerr << listQuery[i] << std::endl;
+        }
+
 
         std::string outstr("Content-type: text/html\r\n"
             "\r\n"
@@ -143,7 +276,20 @@ int main(void)
             "</html>\n");
         request.putstr(outstr);
     }
+private:
+};
 
+
+int main(void)
+{
+    FCGX_Init();
+    RequestManager requestManager;
+
+    Request request;
+    while (request.accept() == 0)
+    {
+        requestManager.handleRequest(request);
+    }
 
     return 0;
 }
