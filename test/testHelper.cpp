55f7598db5976911ebebf7c59aad048ec92f6a4a
David Beck <david.beck@bexoft.de>
2025-12-08 08:35:45 +0100

    Fix testHelper.cpp

--- test/testHelper.cpp
+++ test/testHelper.cpp
@@ -37,9 +37,9 @@ bool waitTillDone(testing::internal::ExpectationBase& expectation, int waittime)
 {
     for (int i = 0; i < (waittime / 10); ++i)
     {
-        testing::internal::g_gmock_mutex.Lock();
+        testing::internal::g_gmock_mutex.lock();
         bool ok = expectation.IsSaturated();
-        testing::internal::g_gmock_mutex.Unlock();
+        testing::internal::g_gmock_mutex.unlock();
         if (ok)
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(5));
